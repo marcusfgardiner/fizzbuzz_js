1359876f307c98cfb4aa1ff4c4ac44629e22282d
Stephen Tilley <stephentilley@gmail.com>
2018-02-19 17:47:51 +0000

    Prints fizzbuzz to the console

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -16,3 +16,9 @@ FizzBuzz.prototype.play = function(number) {
 FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
   return (number % divisor === 0);
 }
+
+var fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
+}
