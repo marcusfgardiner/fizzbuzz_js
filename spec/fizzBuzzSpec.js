52b44fc25ecb6e9baee2676532d6cd5d05c6886e
Stephen Tilley <stephentilley@gmail.com>
2018-02-19 17:38:14 +0000

    FizzBuzz complete, tested and passing

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,15 +1,37 @@
 describe("Fizzbuzz", function() {
 
-  it("should print fizz when passed 3", function() {
-  expect(fizzBuzz(3)).toEqual('fizz');
-});
+  var fizzBuzz;
 
-  it ("should print buzz when passed 5", function() {
-    expect(fizzBuzz(5)).toEqual('buzz');
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
   });
 
-  it("should print buzz when passed 10", function() {
-    expect(fizzBuzz(7)).toEqual(7);
+  it("should print fizz when passed 3", function() {
+    expect(fizzBuzz.play(3)).toEqual('fizz');
+  });
+
+  it("should print fizz when passed a multiple 3", function() {
+    expect(fizzBuzz.play(96)).toEqual('fizz');
+  });
+
+  it ("should print buzz when passed 5", function() {
+    expect(fizzBuzz.play(5)).toEqual('buzz');
+  });
+
+  it ("should print buzz when passed a multiple of 5", function() {
+    expect(fizzBuzz.play(65)).toEqual('buzz');
+  });
+
+  it("should print numbers that are not a multiples of 3 or 5", function() {
+    expect(fizzBuzz.play(7)).toEqual(7);
+  });
+
+  it("should print fizzbuzz when passed 15", function() {
+    expect(fizzBuzz.play(15)).toEqual("fizzbuzz");
+  });
+
+  it("should print fizzbuzz when passed a multiple of 15", function() {
+    expect(fizzBuzz.play(105)).toEqual("fizzbuzz");
   });
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,10 +1,18 @@
-function fizzBuzz(number) {
-  if (number % 3 === 0) {
-    return 'fizz' ;
-  } else if (number % 5 === 0) {
-    return 'buzz' ;
+function FizzBuzz() {
+}
+
+FizzBuzz.prototype.play = function(number) {
+  if (this._isDivisibleBy(15, number)) {
+    return 'fizzbuzz';
+  } else if (this._isDivisibleBy(5, number)) {
+    return 'buzz';
+  } else if (this._isDivisibleBy(3, number)) {
+    return 'fizz';
+  } else {
+    return number;
   }
-  else {
-    return number ;
-  }
+}
+
+FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+  return (number % divisor === 0);
 }
